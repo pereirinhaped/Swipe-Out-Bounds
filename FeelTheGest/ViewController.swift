4246a5bbbab7c14d39c1b51d4ee2a544acb11930
Pedro Pereirinha <pereirinhaped@gmail.com>
2016-08-08 15:24:08 +0100

    3 subviews added to scrollView

--- FeelTheGest/ViewController.swift
+++ FeelTheGest/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var mainScrollView: UIScrollView!
+	
+	let bkgColors = [UIColor.yellow.withAlphaComponent(0.3), UIColor.blue.withAlphaComponent(0.3), UIColor.red.withAlphaComponent(0.3)]
+	
+	var MAX_PAGE: Int {
+		return bkgColors.count
+	}
+	let MIN_PAGE = 0
+	var CUR_PAGE = 0
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
+		
+		// Set some ScrollView properties
+		mainScrollView.backgroundColor = UIColor.purple.withAlphaComponent(0.3)
+		mainScrollView.isScrollEnabled = true
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
+	
+	override func viewDidAppear(_ animated: Bool) {
+		
+		for x in 0...2 {
+			
+			let scrollViewWidth = mainScrollView.frame.width
+			let scrollViewHeight = mainScrollView.frame.height
+			
+			let newX = scrollViewWidth * (1/2.0 + CGFloat(x))
+			
+			let subView = UIView(frame: CGRect(x: newX - 50, y: (scrollViewHeight / 2) - 50, width: 100, height: 100))
+			subView.backgroundColor = bkgColors[x]
+			
+			mainScrollView.addSubview(subView)
+		}
+		
+		let contentWidth = mainScrollView.frame.width * CGFloat(bkgColors.count)
+		let contentHeight = mainScrollView.frame.height
+		mainScrollView.contentSize = CGSize(width: contentWidth, height: contentHeight)
+	}
 
 }
 
